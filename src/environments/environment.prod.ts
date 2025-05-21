ffa1fb001847aa6b33480bb7ef09503f083112ca
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-05-21 20:43:10 -0300

    Adiciona configurações do Supabase ao ambiente de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  supabaseUrl: 'https://slfygxknqrsemlhfvguq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZnlneGtucXJzZW1saGZ2Z3VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4Njg4MjksImV4cCI6MjA2MzQ0NDgyOX0.hYjIlLDuqyaSWQ5ttoyb934wso12SKlyoBUtVLk0ou4',
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://slfygxknqrsemlhfvguq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZnlneGtucXJzZW1saGZ2Z3VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4Njg4MjksImV4cCI6MjA2MzQ0NDgyOX0.hYjIlLDuqyaSWQ5ttoyb934wso12SKlyoBUtVLk0ou4',
 };
 
 /*
